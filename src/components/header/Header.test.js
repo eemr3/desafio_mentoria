fb98c246a2ed5a30f337914db2ecdc318d18de07
Emerson <eemr3@yahoo.com.br>
2021-01-12 16:52:30 -0300

    test(header): Testando rendereização do header

---
+++ src/components/header/Header.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { createMemoryHistory } from 'history'
+import React from 'react'
+import { Router } from 'react-router-dom'
+
+import '@testing-library/jest-dom/extend-expect'
+
+import Header from './Header'
+
+it('navigate form Aluno/Turma', () => {
+  const history = createMemoryHistory()
+  render(
+    <Router history={history}>
+      <Header />
+    </Router>
+  )
+
+  expect(screen.getByText(/Desenvolvendo-ME/i)).toBeInTheDocument()
+
+  const leftClick = { button: 0 }
+  userEvent.click(screen.getByText(/Matricula/i), leftClick)
+
+  expect(screen.getByText(/Desenvolvendo-ME/i)).toBeInTheDocument()
+})
